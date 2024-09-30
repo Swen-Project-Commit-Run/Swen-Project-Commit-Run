91d71569343ff59a9f0b16441e83b74e292eac6a
Lorenzo Gould-Davies <lorenzogouldd@gmail.com>
2024-09-30 06:28:03 +0000

    job application model added

---
+++ App/models/JobApplications.py
@@ -0,0 +1,21 @@
+from App.database import db
+
+class JobApplication(db.model):
+    __tablename__='Job_Applications'
+    id=db.Column(db.Integer, primary_key=True)
+    jobId =db.Column(db.Integer, db.ForeignKey('job_listing.id'), nullable=False)
+    applicantId = db.Collumn(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    jobListing = db.relationship('JobListing', backref='jobApplications')
+    applicant = db.relationship('Applicant', backref='applicantApplication')
+
+    def __init__(self, jobId, applicantId):
+        self.jobId = jobId
+        self.applicantId = applicantID
+
+    def to_json(self):
+        return{
+            'id': self.id,
+            'job_id': self.jobId,
+            'applicant_id': self.applicant_id
+        }
--- App/models/JobListing.py
+++ App/models/JobListing.py
@@ -6,19 +6,19 @@ class JobListing(db.model):
     id= db.Column(db.Interger, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     description = db.Column(db.text, nullable=False)
-    employer_id = db.column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    employerId = db.column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
     employer = db.relationship('Employer', backref='job_listings')
 
-    def __init__(self, title, description, employer_id):
+    def __init__(self, title, description, employerId):
         self.title = title
         self.description = description
-        self.employer_id = employer_id
+        self.employerId = employerId
 
     def to_json(self):
         return{
             'id': self.id,
             'title': self.title,
             'description': self.description,
-            'employer_id' : self.employer_id
+            'employer_id' : self.employerId
         }
